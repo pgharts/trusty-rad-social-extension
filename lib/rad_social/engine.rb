196fc058c3f26ba04cbf02740fed56e366334fe5
Eric Sipple <saalon@gmail.com>
2014-12-30 13:25:20 -0500

    Fixing compilation of css.

--- lib/rad_social/engine.rb
+++ lib/rad_social/engine.rb
@@ -1,5 +1,10 @@
 module RadSocial
   class Engine < Rails::Engine
     paths["app/helpers"] = []
+
+    initializer "trusty_cms.assets.precompile" do |app|
+      app.config.assets.precompile += %w(rad_sociala/rad_screen.css rad_social/captcha.js rad_social/rad_ajax_form.js rad_social/rad_email_form.js rad_social/rad_email_validator.js rad_social/rad_widget.js)
+    end
+
   end
 end
--- lib/trusty-rad-social-extension.rb
+++ lib/trusty-rad-social-extension.rb
@@ -1,5 +1,5 @@
 module TrustyRadSocialExtension
-  VERSION     = "1.0.3"
+  VERSION     = "1.0.4"
   SUMMARY     = "Rad Social for Trusty CMS"
   DESCRIPTION = "Makes Trusty CMS better by adding rad_social!"
   URL         = "http://example.com/rad_social"

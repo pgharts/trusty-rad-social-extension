01e333c00ea480112fa1d65d1fb3d2d18c744490
Eric Sipple <saalon@gmail.com>
2014-12-30 13:33:28 -0500

    1.0.5

--- lib/trusty-rad-social-extension.rb
+++ lib/trusty-rad-social-extension.rb
@@ -1,5 +1,5 @@
 module TrustyRadSocialExtension
-  VERSION     = "1.0.4"
+  VERSION     = "1.0.5"
   SUMMARY     = "Rad Social for Trusty CMS"
   DESCRIPTION = "Makes Trusty CMS better by adding rad_social!"
   URL         = "http://example.com/rad_social"

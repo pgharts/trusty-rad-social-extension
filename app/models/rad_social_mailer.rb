bc69ebbfb1584bc7030462a2ba2669e4e6a321f5
Brittany Martin <brittany.jill.martin@gmail.com>
2016-02-01 11:22:17 -0500

    Move RadSocial to using Roadie-Rails

--- app/models/rad_social_mailer.rb
+++ app/models/rad_social_mailer.rb
@@ -1,16 +1,17 @@
 require 'mail'
 class RadSocialMailer < ActionMailer::Base
+  include Roadie::Rails::Automatic
 
   def social_mail options
-
-    #binding.pry
-
     from_address = Mail::Address.new options[:from] # ex: "john@example.com"
     from_address.display_name = options[:from_name] # ex: "John Doe"
-    mail(:to => options[:to], :from => from_address.format, :subject => options[:subject]) do |format|
-      format.html {render :text => options[:message]}
-    end
 
+    mail({
+      to: options[:to],
+      from: from_address.format,
+      subject: options[:subject],
+      text: options[:message],
+      content_type: "text/html"
+     })
   end
-
 end

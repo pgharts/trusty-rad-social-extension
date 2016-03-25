c4422aa1119efc3e76e7c345dbf3396eba482228
Brittany Martin <brittany.jill.martin@gmail.com>
2016-03-25 13:05:55 -0400

    Upgrade Trusty CMS and move to version 2.2.1

--- lib/trusty-rad-social-extension.rb
+++ lib/trusty-rad-social-extension.rb
@@ -1,5 +1,5 @@
 module TrustyRadSocialExtension
-  VERSION     = "2.2.0"
+  VERSION     = "2.2.1"
   SUMMARY     = "Rad Social for Trusty CMS"
   DESCRIPTION = "Makes Trusty CMS better by adding rad_social!"
   URL         = "http://example.com/rad_social"

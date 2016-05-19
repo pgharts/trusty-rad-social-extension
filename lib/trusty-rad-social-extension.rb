4b31cb14b86c60cc3deae7d7b70f8900ffe6aaa2
Brittany Martin <brittany.jill.martin@gmail.com>
2016-05-19 14:30:41 -0400

    See bugzid: 3423 - Twitter window is missing a target=_blank in the link

--- lib/trusty-rad-social-extension.rb
+++ lib/trusty-rad-social-extension.rb
@@ -1,5 +1,5 @@
 module TrustyRadSocialExtension
-  VERSION     = "2.2.2"
+  VERSION     = "2.2.3"
   SUMMARY     = "Rad Social for Trusty CMS"
   DESCRIPTION = "Makes Trusty CMS better by adding rad_social!"
   URL         = "http://example.com/rad_social"

728d6ecaa9623250abe00d7a5d184ef12e872841
Brittany Martin <brittany.jill.martin@gmail.com>
2016-06-06 12:13:01 -0400

    Register version 2.2.4

--- lib/trusty-rad-social-extension.rb
+++ lib/trusty-rad-social-extension.rb
@@ -1,5 +1,5 @@
 module TrustyRadSocialExtension
-  VERSION     = "2.2.3"
+  VERSION     = "2.2.4"
   SUMMARY     = "Rad Social for Trusty CMS"
   DESCRIPTION = "Makes Trusty CMS better by adding rad_social!"
   URL         = "http://example.com/rad_social"

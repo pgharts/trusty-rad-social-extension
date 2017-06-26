a20eecf230e1f40778fc3a6180189d7f0a187ccf
Danielle Greaves <danigreaves@gmail.com>
2017-06-26 13:23:41 -0400

    BugzID: 4130 - Registering trusty-rad 2.2.7

--- lib/trusty-rad-social-extension.rb
+++ lib/trusty-rad-social-extension.rb
@@ -1,5 +1,5 @@
 module TrustyRadSocialExtension
-  VERSION     = "2.2.6"
+  VERSION     = "2.2.7"
   SUMMARY     = "Rad Social for Trusty CMS"
   DESCRIPTION = "Makes Trusty CMS better by adding rad_social!"
   URL         = "http://example.com/rad_social"

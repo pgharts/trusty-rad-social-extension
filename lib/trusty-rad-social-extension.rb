a22d60de2d2d47757b0fac994eaaa931f47539dc
Patrick FitzGerald <fitzgerald@trustarts.org>
2017-06-27 07:34:09 -0400

    URL was incorrect

--- lib/trusty-rad-social-extension.rb
+++ lib/trusty-rad-social-extension.rb
@@ -2,7 +2,7 @@ module TrustyRadSocialExtension
   VERSION     = "3.0.0"
   SUMMARY     = "Rad Social for Trusty CMS"
   DESCRIPTION = "Makes Trusty CMS better by adding rad_social!"
-  URL         = "http://example.com/rad_social"
+  URL         = "https://github.com/pgharts/trusty-rad-social-extension"
   AUTHORS     = ["Eric Sipple, Brittany Martin, Danielle Greaves"]
   EMAIL       = ["sipple@trustarts.org, martin@trustarts.org, greaves@trustarts.org"]
 end
